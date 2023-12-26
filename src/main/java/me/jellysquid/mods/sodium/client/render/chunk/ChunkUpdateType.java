5f376579510bdcaf5d922f18614f46ead1f0ec20
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-26 15:15:00 -0300

    Update Chunks

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/ChunkUpdateType.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/ChunkUpdateType.java
@@ -1,22 +1,21 @@
 package me.jellysquid.mods.sodium.client.render.chunk;
 
 public enum ChunkUpdateType {
-    INITIAL_BUILD(128),
-    REBUILD(Integer.MAX_VALUE),
+    INITIAL_BUILD(64),
+    REBUILD(64),
     IMPORTANT_REBUILD(Integer.MAX_VALUE);
 
-    private final int maximumQueueSize;
+    private static final int MAX_QUEUE_SIZE = 64;
 
-    ChunkUpdateType(int maximumQueueSize) {
-        this.maximumQueueSize = maximumQueueSize;
+    ChunkUpdateType() {
     }
 
     public static boolean canPromote(ChunkUpdateType prev, ChunkUpdateType next) {
-        return prev == null || (prev == REBUILD && next == IMPORTANT_REBUILD);
+        return prev != IMPORTANT_REBUILD && next == IMPORTANT_REBUILD;
     }
 
     public int getMaximumQueueSize() {
-        return this.maximumQueueSize;
+        return MAX_QUEUE_SIZE;
     }
 
     public boolean isImportant() {
